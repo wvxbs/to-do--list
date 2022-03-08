588cd6e6d4efe0c9a06c3614d81826060389fe8d
wvxbs <gabriel.ferreira7854@gmail.com>
2022-03-08 16:30:47 -0300

    .

---
+++ src/Pages.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Pages = () => {
+    return (
+        <div className="content">
+            <h1 className="title">
+                oooopa
+            </h1>
+        </div>
+    )
+}
+
+export default Pages
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Pages from './Pages'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Pages />
   </React.StrictMode>,
   document.getElementById('root')
 );
